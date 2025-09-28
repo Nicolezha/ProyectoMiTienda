64414629dd2802f0dde98a105fc3af05ddb86e31
Nicolezha <zharicknicoleha@ufps.edu.co>
2025-09-27 21:06:55 -0500

    otro

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
-  build: {
-    outDir: "../backend/static", // <-- así los assets van directo a la carpeta estática de Flask
-  },
-  base: "/static/", // <-- rutas absolutas para producción
-});
+export default defineConfig({ plugins: [react()], server: { port: 5173 } });
